2b6588f01cbc220025a3fbbd8ea68113c5efbc26
Murilo <murilpcarneiro@gmail.com>
2025-07-02 20:54:14 -0300

    feat: adiciona a lógica do login no controller

--- backend/src/interfaces/controllers/UserController.ts
+++ backend/src/interfaces/controllers/UserController.ts
@@ -1,3 +1,4 @@
+import { LoginUser } from 'app/use-cases/user/LoginUser';
 import { RegisterUser } from 'app/use-cases/user/RegisterUser';
 import { NextFunction, Request, Response } from 'express';
 import { PostgresUserRepository } from 'infra/repositories/PostgresUserRepository';
@@ -14,6 +15,20 @@ export class UserController {
       return res.status(201).json(result);
     } catch (error) {
       next(error);
+    }
+  }
+
+  static async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const {email, password} = req.body;
+  
+      const userRepo = new PostgresUserRepository();
+      const useCase = new LoginUser(userRepo);
+      const result = await useCase.execute({ email, password });
+  
+      return res.status(200).json(result)
+    } catch (err) {
+      next(err);
     }
   }
 }
