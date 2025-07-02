407dfe66728f2e532e17c453642e6c006d7e6fd0
Murilo <murilpcarneiro@gmail.com>
2025-07-02 17:10:57 -0300

    feat: cria a rota de register

---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import { UserController } from 'interfaces/controllers/UserController';
+
+const router = Router();
+
+router.post('/register', (req: Request, res: Response, next: NextFunction) => {
+  UserController.register(req, res, next);
+});
+
+export default router;
